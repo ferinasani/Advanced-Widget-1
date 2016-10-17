e25bf79f44a0e2dbf8881554ebace76f4b695d35
FaniFerinaSani/17/XIIR5 <ferinasani79@gmail.com>
2016-10-17 12:38:55 +0700

    .

--- app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget1/MainActivity.java
@@ -1,7 +1,7 @@
 package id.sch.smktelkom_mlg.learn.advancedwidget1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
@@ -95,6 +95,4 @@ public class MainActivity extends AppCompatActivity
 
         tvHasil.setText(hasil);
     }
-
-
 }
